96ad9cac7b374d2b514a0f6a15de5f582b6abd41
AmelCG <amel.delgado2002@gmail.com>
2023-11-27 19:54:20 -0600

    A

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,17 @@
-﻿namespace DotNote2
+﻿using DotNote2.Models;
+
+namespace DotNote2
 {
     public partial class App : Application
     {
+        /// <summary>
+        /// Usuario logueado. <br/>
+        /// En el inicio de sesion se define su valor para ser usado en los otros view.
+        /// </summary>
+        public static Usuario Usuario { get; set; }
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
     }
