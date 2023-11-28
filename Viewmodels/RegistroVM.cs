c2f8dba1faaa5b4d95cd35aa9f7d14fadfc915f3
Samantha <samcalderon2510@gmail.com>
2023-11-27 23:55:40 -0600

    crear registroviewmodel

---
+++ Viewmodels/RegistroVM.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DotNote2.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNote2.Viewmodels
+{
+    public partial class RegistroVM :  BaseViewmodel.BaseViewmodel
+    {
+        SQLiteService sqlite;
+
+        [ObservableProperty]
+        public string username;
+
+        [ObservableProperty]
+        public string email;
+
+        [ObservableProperty]
+        public string password;
+
+        public RegistroVM()
+        {
+            sqlite = new SQLiteService();
+            Username = string.Empty;
+            Email = string.Empty;
+            Password = string.Empty;
+        }
+        [RelayCommand]
+        public async Task Registrar()
+        {
+
+        }
+
+    }
+}
