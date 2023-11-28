add0553df0c4b9718599548ec8c92df3709775bd
AmelCG <amel.delgado2002@gmail.com>
2023-11-27 18:37:56 -0600

    Modelo de usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using SQLite;
+
+namespace DotNote2.Models
+{
+    /// <summary>
+    /// Modelo que describe la estructura de un usuario
+    /// </summary>
+    public class Usuario
+    {
+        /// <summary>
+        /// Identificador del usuario. <br/>
+        /// Numero entero de valor unico.
+        /// </summary>
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nombre y Apellido del usuario. <br />
+        /// <i>Puede ser simplemente el nombre sin el apellido.</i>
+        /// </summary>
+        public string NameSurname { get; set; }
+
+        /// <summary>
+        /// Nombre de usuario para iniciar sesión. <br/>
+        /// Debe ser unico.
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Contraseña secreta para iniciar sesión.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Correo electronico del usuario. <br/>
+        /// Sirve para enviar notas por correo.
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
