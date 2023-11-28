a275d09371883986da5b1f2466cc2c01f5920640
Samantha <samcalderon2510@gmail.com>
2023-11-27 22:52:26 -0600

    inicio sesion view moel

---
+++ Viewmodels/InicioSesionVM.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DotNote2.Servicios;
+using DotNote2.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNote2.Viewmodels
+{
+    public partial class InicioSesionVM : BaseViewmodel.BaseViewmodel
+    {
+        SQLiteService sqlite;
+
+        [ObservableProperty]
+        public string username;
+
+        [ObservableProperty]
+        public string password;
+
+        public InicioSesionVM()
+        {
+            sqlite = new SQLiteService();
+            Username = string.Empty;
+            Password = string.Empty;
+        }
+
+        [RelayCommand]
+        public async Task IniciarSesion()
+        {
+            var usuario = await sqlite.IniciarSesionAsync(Username,Password);
+            if (usuario != null)
+            {
+                App.Usuario = usuario;
+                await SecureStorage.Default.SetAsync("usuario",usuario.Username);
+                await SecureStorage.Default.SetAsync("password",usuario.Password);
+                await Application.Current.MainPage.Navigation.PushAsync(new MisNotas());
+            }
+        }
+        [RelayCommand]
+        public async Task IrRegistrar()
+        {
+            
+        }
+    }
+}
