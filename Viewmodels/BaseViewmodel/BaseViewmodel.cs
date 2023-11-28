95c5e2ca246fd0abfc516bb9b070e3ff63c63d51
AmelCG <amel.delgado2002@gmail.com>
2023-11-27 18:59:16 -0600

    Creacion de base viewmodel

---
+++ Viewmodels/BaseViewmodel/BaseViewmodel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DotNote2.Viewmodels.BaseViewmodel
+{
+    /// <summary>
+    /// Clase base para todos los viewmodels
+    /// </summary>
+    public partial class BaseViewmodel : ObservableObject
+    {
+        /// <summary>
+        /// Propiedad que devuelve <i>true</i> si esta ocupado el viewmodel. <br/>
+        /// En otro caso devuelve <i>false</i>. <br/>
+        /// Notifica IsNotBusy por cualquier cambio que este sufra.
+        /// </summary>
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        public bool isBusy;
+
+        /// <summary>
+        /// Devuelve si el viewmodel <i>true</i> si no esta ocupado. <br/>
+        /// En otro caso es <i>false</i>.
+        /// </summary>
+        public bool IsNotBusy => !IsBusy;
+    }
+}
